4c1ac2c3f85c186bea0bc59bff23bb4f8987d73d
Dennys-27 <dennismarlon14@gmail.com>
2024-12-07 10:46:42 -0500

    APLICACION GENERADOR DE CERTIFICADOS

---
+++ view/Html/panel.php
@@ -0,0 +1,36 @@
+<!-- ########## START: HEAD PANEL ########## -->
+<div class="br-header">
+      <div class="br-header-left">
+        <div class="navicon-left hidden-md-down"><a id="btnLeftMenu" href=""><i class="icon ion-navicon-round"></i></a></div>
+        <div class="navicon-left hidden-lg-up"><a id="btnLeftMenuMobile" href=""><i class="icon ion-navicon-round"></i></a></div>
+        <div class="input-group hidden-xs-down wd-170 transition">
+          <input id="searchbox" type="text" class="form-control" placeholder="Search">
+          <span class="input-group-btn">
+            <button class="btn btn-secondary" type="button"><i class="fa fa-search"></i></button>
+          </span>
+        </div><!-- input-group -->
+      </div><!-- br-header-left -->
+      <div class="br-header-right">
+        <nav class="nav">
+         
+          <div class="dropdown">
+            <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
+              <span class="logged-name hidden-md-down">Dennys Marlon Tierra Alvarado</span>
+              <img src="http://via.placeholder.com/64x64" class="wd-32 rounded-circle" alt="">
+              <span class="square-10 bg-success"></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-header wd-200">
+              <ul class="list-unstyled user-profile-nav">
+                <li><a href=""><i class="icon ion-ios-person"></i> Editar Perfil</a></li>
+                <li><a href=""><i class="icon ion-ios-gear"></i> Configuraciones</a></li>
+                <li><a href=""><i class="icon ion-ios-download"></i> Certificados</a></li>
+                
+                <li><a href=""><i class="icon ion-power"></i> Salir</a></li>
+              </ul>
+            </div><!-- dropdown-menu -->
+          </div><!-- dropdown -->
+        </nav>
+        
+      </div><!-- br-header-right -->
+    </div><!-- br-header -->
+    <!-- ########## END: HEAD PANEL ########## -->
---
+++ view/UsuHome/index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include_once("../Html/head.php")?>
+    <title>Home</title>
+  </head>
+
+  <body>
+
+    <!-- ########## START: LEFT PANEL ########## -->
+    <div class="br-logo"><a href=""><span>[</span>bracket<span>]</span></a></div>
+    <?php include_once("../Html/menu.php")?>
+    <!-- ########## END: LEFT PANEL ########## -->
+    <?php include_once("../Html/panel.php")?>
+    
+
+    
+
+    <!-- ########## START: MAIN PANEL ########## -->
+    <div class="br-mainpanel">
+      <div class="br-pageheader pd-y-15 pd-l-20">
+        <nav class="breadcrumb pd-0 mg-0 tx-12">
+          <a class="breadcrumb-item" href="index.html">Bracket</a>
+          <span class="breadcrumb-item active">Blank Page</span>
+        </nav>
+      </div><!-- br-pageheader -->
+      <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+        <h4 class="tx-gray-800 mg-b-5">Blank Page (Default Layout)</h4>
+        <p class="mg-b-0">Introducing Bracket admin template, the most handsome admin template of all time.</p>
+      </div>
+
+      <div class="br-pagebody">
+
+        <!-- start you own content here -->
+
+      </div><!-- br-pagebody -->
+
+    </div><!-- br-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+    <?php include_once("../Html/js.php")?>
+   
+  </body>
+</html>
